0e1cf78bc3b54127ec4debac36d279e87e43e2bb
zueleide6 <161615388+zueleide6@users.noreply.github.com>
2024-03-05 00:50:03 -0300

    Update EmpresaTable.js

--- ligadores_front/src/EmpresaTable.js
+++ ligadores_front/src/EmpresaTable.js
@@ -39,7 +39,7 @@ function EmpresaTable() {
           onChange={handleBancoChange}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         >
-          <option value="">Itau</option>
+          <option value="">Todos</option>
      {bancos.map((banco) => (
             <option key={banco} value={banco}>{banco}</option>
           ))}
