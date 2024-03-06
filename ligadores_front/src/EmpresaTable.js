a853307e559deb24307e459dc9fd4c1ff068bb90
zueleide6 <161615388+zueleide6@users.noreply.github.com>
2024-03-05 23:51:37 -0300

    Update EmpresaTable.js31

--- ligadores_front/src/EmpresaTable.js
+++ ligadores_front/src/EmpresaTable.js
@@ -28,7 +28,7 @@ function EmpresaTable() {
     <div className="container mx-auto">
       <div class="bg-white py-24 sm:py-32">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
-          <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+          <dl class="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-1">
             <div class="mx-auto flex max-w-xs flex-col gap-y-4">
               <dt class="text-base leading-7 text-gray-600">qtdade de registros</dt>
               <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">{empresas.length}</dd>
